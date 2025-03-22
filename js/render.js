705668cea151b06b12ba93a498f1d8b320eec9ce
AshChord <jhyn768@naver.com>
2025-03-22 18:15:56 +0900

    404.html 실험

--- js/render.js
+++ js/render.js
@@ -54,11 +54,60 @@ async function renderContent(post) {
   pagination.innerHTML = '';
 
   // .content에 게시물 내용 렌더링
-  const response = await fetch(`/posts/${post.title}.md`);
+  const response = await fetch(`/data/${post.title}.md`);
   const markdownContent = await response.text();
 
   content.innerHTML = marked.parse(markdownContent);
   hljs.highlightAll();
+
+// 코드 블록에 복사 버튼 추가
+content.querySelectorAll("pre").forEach((pre) => {
+  const code = pre.textContent; // 코드 내용 가져오기
+
+  // 복사 버튼 생성
+  const copyButton = document.createElement("button");
+  copyButton.classList.add("copy-button");
+
+  // 복사 버튼 클릭 이벤트
+  copyButton.addEventListener("click", async function (event) {
+    await navigator.clipboard.writeText(code); // 클립보드에 코드 복사
+
+    // 버튼 위치 계산
+    const buttonRect = copyButton.getBoundingClientRect();
+    const buttonTop = buttonRect.top + window.scrollY; // 버튼의 상단 위치
+    const buttonLeft = buttonRect.left + window.scrollX; // 버튼의 좌측 위치
+
+    // 복사 성공 메시지 (toast) 생성
+    const toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.textContent = '복사함';
+    
+    // 토스트 메시지 위치 설정
+    toast.style.position = 'absolute'; // 절대 위치
+    toast.style.top = `${buttonTop - 50}px`; // 버튼보다 50px 위
+    toast.style.left = `${buttonLeft + buttonRect.width / 2 - toast.offsetWidth / 2}px`; // 버튼 중앙에 맞춤
+
+    content.appendChild(toast); // content 요소에 추가
+
+    // 1초 후에 토스트 메시지 보이도록 클래스 추가
+    setTimeout(() => {
+      toast.classList.add('show');
+    }, 100);
+
+    // 3초 후에 메시지 숨기기
+    setTimeout(() => {
+      toast.classList.remove('show');
+    }, 1000); // 메시지가 3초 동안 보인 후
+
+    // 3.5초 후에 실제로 toast 요소를 제거
+    setTimeout(() => {
+      toast.remove();
+    }, 1500); // 애니메이션이 끝난 후 제거
+  });
+
+  // pre 요소 안에 복사 버튼 삽입
+  pre.appendChild(copyButton);
+});
 
   // 테이블 감싸는 div 추가
   const tables = content.querySelectorAll("table");
@@ -134,3 +183,16 @@ function renderBlogCategory() {
     categoryContainer.appendChild(categoryItem);
   });
 }
+
+document.querySelector(".blog-title").addEventListener("click", () => {
+  window.history.pushState({}, "", "/"); // URL 변경
+  router(); // 라우터 실행
+});
+
+document.querySelectorAll(".menu-item").forEach((item) => {
+  item.addEventListener("click", () => {
+      const path = item.textContent.toLowerCase(); // 텍스트를 URL 경로로 변환
+      window.history.pushState({}, "", `/${path}`); // URL 변경
+      router(); // 라우터 실행
+  });
+});
