27866ecc045dc74f9e119cc44100fb3b8a4c80cc
AshChord <jhyn768@naver.com>
2026-07-10 20:41:55 +0900

    wfe

--- js/hydrator.js
+++ js/hydrator.js
@@ -1,14 +1,10 @@
 (async () => {
-  const markdownBody = document.querySelector('body');
-  const currentContentNodes = Array.from(markdownBody.childNodes);
+  const content = Array.from(document.body.childNodes);
 
-  const response = await fetch('/index.html');
-  const htmlText = await response.text();
-
+  const htmlText = await fetch('/index.html').then(res => res.text());
   const newDoc = new DOMParser().parseFromString(htmlText, 'text/html');
 
-  newDoc.querySelector('.content-body').append(...currentContentNodes);
-
+  newDoc.querySelector('.content-body').append(...content);
   document.body.replaceWith(newDoc.body);
 
   for (const oldScript of document.body.querySelectorAll('script')) {
