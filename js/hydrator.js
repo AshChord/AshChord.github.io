de509a26f897d174112e9a73ac0ac513f40c841f
AshChord <jhyn768@naver.com>
2026-07-23 20:34:16 +0900

    hydrate with thumbnail

--- js/hydrator.js
+++ js/hydrator.js
@@ -2,7 +2,19 @@
   const skeletonSource = await fetch('/').then(res => res.text());
   const skeleton = new DOMParser().parseFromString(skeletonSource, 'text/html');
 
-  skeleton.querySelector('.content-body').append(...document.body.childNodes);
+  const thumbnail = document.body.querySelector('.thumbnail');
+
+  if (thumbnail) {
+    while (true) {
+      const node = document.body.firstChild;
+      skeleton.querySelector('.content-header').appendChild(node);
+      if (node === thumbnail.parentElement) break;
+    }
+    skeleton.querySelector('.content-body').append(...document.body.childNodes);
+  } else {
+    skeleton.querySelector('.content').replaceChildren(...document.body.childNodes);
+  }
+
   document.body.replaceWith(skeleton.body);
 
   for (const inertScript of document.body.querySelectorAll('script')) {
