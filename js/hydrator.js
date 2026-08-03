8031e377b6c249883930eb509f03d1063045049e
AshChord <jhyn768@naver.com>
2026-08-03 21:54:56 +0900

    ㅗ됴

--- js/hydrator.js
+++ js/hydrator.js
@@ -4,14 +4,11 @@ document.firstChild.remove();
   const skeletonSource = await fetch('/').then(res => res.text());
   const skeleton = new DOMParser().parseFromString(skeletonSource, 'text/html');
 
-  const marker = document.querySelector('.end');
+  const nodes = [...document.body.childNodes];
+  const marker = nodes.find(n => n.nodeType === 8 && !n.data);
+  const contentNodes = nodes.slice(nodes.indexOf(marker) + 1);
 
-  while (document.body.firstChild !== marker)
-    document.body.firstChild.remove();
-
-  marker.remove();
-
-  skeleton.querySelector('.content-body').append(...document.body.childNodes);
+  skeleton.querySelector('.content-body').append(...contentNodes);
   document.body.replaceWith(skeleton.body);
 
   for (const inertScript of document.body.querySelectorAll('script')) {
