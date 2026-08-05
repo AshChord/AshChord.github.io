1039b09a3375d83dca7dc992da8d1b289005e180
AshChord <jhyn768@naver.com>
2026-08-05 16:38:23 +0900

    shiki 시작

---
+++ js/highliter.js
@@ -0,0 +1,31 @@
+// highlighter.js
+import { createHighlighter } from "https://esm.sh/shiki";
+
+// 언어 로딩 상태 저장
+const languageCache = new Map();
+
+// Shiki 초기화 Promise
+const highlighterPromise = createHighlighter({
+  themes: ["github-light"],
+  langs: ["plaintext"]
+});
+
+
+// 전역 API 등록
+window.Highlighter = {
+
+  async get() {
+    return await highlighterPromise;
+  },
+
+  async loadLanguage(lang) {
+    const highlighter = await highlighterPromise;
+
+    if (!languageCache.has(lang)) {
+      const loading = highlighter.loadLanguage(lang);
+      languageCache.set(lang, loading);
+    }
+
+    await languageCache.get(lang);
+  }
+};
